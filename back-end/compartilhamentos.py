972f8904f4e4397875b83e4ee6737c8da3546ac7
gustavomachado112 <159536397+gustavomachado112@users.noreply.github.com>
2025-07-21 19:09:26 -0300

    Create compartilhamentos.py

---
+++ back-end/compartilhamentos.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+@dataclass
+class Compartilhamento:
+    compartilhamento_id: int
+    agenda_id: int
+    usuario: str
+    compartilhar_tarefa: str
+    permissao: str  # valores possíveis: leitura, escrita, total
+
+def cadastrar_compartilhamento():
+    try:
+        compartilhamento_id = int(input("ID da tarefa que voce quer compartilhar: "))
+        agenda_id = int(input("ID da Agenda: "))
+        usuario = input("Usuário: ")
+        compartilhar_tarefa = input("Compartilhar tarefa (nome da tarefa): ")
+        permissao = input("Permissão (leitura, escrita ou total): ").lower()
+
+        permissoes_validas = ["leitura", "escrita", "total"]
+        if permissao not in permissoes_validas:
+            print("negado , so pode usar leitura , escritaou totla")
+            return
+
+        compartilhamento = Compartilhamento(
+            compartilhamento_id=compartilhamento_id,
+            agenda_id=agenda_id,
+            usuario=usuario,
+            compartilhar_tarefa=compartilhar_tarefa,
+            permissao=permissao
+        )
+
+        print("\nCompartilhamento feito com sucesso , xandao esta feliz com voce!")
+        print(compartilhamento)
+
+    except ValueError:
+        print("Erro: digite numero validos , se nao xandao fica triste")
